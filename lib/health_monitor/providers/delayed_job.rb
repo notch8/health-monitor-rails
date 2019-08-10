02d6af596927447e6d5a0b77950d920799d440ec
Rob Kaufman <rob@notch8.com>
2019-08-09 22:49:36 -0700

    Add latency to delayed_job

--- lib/health_monitor/providers/delayed_job.rb
+++ lib/health_monitor/providers/delayed_job.rb
@@ -8,16 +8,20 @@ module HealthMonitor
     class DelayedJob < Base
       class Configuration
         DEFAULT_QUEUES_SIZE = 100
+        DEFAULT_LATENCY = 1.hour
 
         attr_accessor :queue_size
+        attr_accessor :latency
 
         def initialize
           @queue_size = DEFAULT_QUEUES_SIZE
+          @latency = DEFAULT_LATENCY
         end
       end
 
       def check!
         check_queue_size!
+        check_latency!
       rescue Exception => e
         raise DelayedJobException.new(e.message)
       end
@@ -38,6 +42,14 @@ module HealthMonitor
         return unless size > configuration.queue_size
 
         raise "queue size #{size} is greater than #{configuration.queue_size}"
+      end
+      
+      def check_latency!
+        # we dont want failed but want both locked and queued
+        oldest = job_class.order(:created_at).where(failed_at: nil).first
+        age = Time.now - oldest.created_at
+        return unless age < configuration.latency
+        raise "latency for #{oldest.id} of #{ActiveSupport::Duration.build(age)} is greater than #{configuration.latency}"
       end
 
       def job_class
