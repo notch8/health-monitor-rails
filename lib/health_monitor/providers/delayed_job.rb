1d9dd18c61aa325fe048490cccc36bb8e9c75041
Rob Kaufman <rob@notch8.com>
2019-08-15 17:36:00 -0700

    Add failure count (#5)
    * Add failure count
    
    max number of failed jobs allowed
    
    * Update readme for new setting

--- lib/health_monitor/providers/delayed_job.rb
+++ lib/health_monitor/providers/delayed_job.rb
@@ -9,19 +9,23 @@ module HealthMonitor
       class Configuration
         DEFAULT_QUEUES_SIZE = 100
         DEFAULT_LATENCY = 1.hour
+        DEFAULT_FAILURES = 0
 
         attr_accessor :queue_size
         attr_accessor :latency
+        attr-accessor :failures
 
         def initialize
           @queue_size = DEFAULT_QUEUES_SIZE
           @latency = DEFAULT_LATENCY
+          @failures = DEFAULT_FAILURES
         end
       end
 
       def check!
         check_queue_size!
         check_latency!
+        check_failures!
       rescue Exception => e
         raise DelayedJobException.new(e.message)
       end
@@ -46,10 +50,16 @@ module HealthMonitor
       
       def check_latency!
         # we dont want failed but want both locked and queued
-        oldest = job_class.order(:created_at).where(failed_at: nil).first
+        oldest = job_class.order(:created_at).where(last_error: nil).first
         age = Time.now - oldest.created_at
         return unless age < configuration.latency
         raise "latency for #{oldest.id} of #{ActiveSupport::Duration.build(age)} is greater than #{configuration.latency}"
+      end
+      
+      def check_failures!
+        failures = job_class.where('last_error IS NOT NULL').count
+        return unless failures > configuration.failures
+        raise "there are #{failures} failed jobs, which is higher than the allowed #{configuration.failures} failures"
       end
 
       def job_class
